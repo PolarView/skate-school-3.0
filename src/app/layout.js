14235b35b98e21f339d2d0ac54e7e16915d6e3b4
Sanchiz <chrisrosy29@mail.ru>
2024-03-23 21:48:30 +0300

    vercel speed inside added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Skateschool - скейт школа в москве",
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="ru">
       <body>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
