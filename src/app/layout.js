b1b75b553f2f9e03686f8148ccdc0cd18f1bf223
Sanchiz <chrisrosy29@mail.ru>
2024-03-24 16:25:31 +0300

    vercel speed inside removed + live translation btn in mobile navbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Skateschool - скейт школа в москве",
@@ -19,7 +18,6 @@ export default function RootLayout({ children }) {
     <html lang="ru">
       <body>{children}</body>
       <Analytics />
-      <SpeedInsights />
     </html>
   );
 }
